be9521c0459ac6a15ec64c53687ce4d67c9df2b1
aslo <gzaltamara@gmail.com>
2026-04-24 14:36:32 -0700

    first commit 2

---
+++ ICS106/22.3list_training.py
@@ -0,0 +1,37 @@
+
+
+
+class commision_clac_by_munth():
+    #the payment will be according month 9 - 5 $
+    # other month 1 $
+    #
+    months = [1,4,5,7,11,9,6,9,10,9,9]
+    payment = 0
+    for month in months :
+        if month ==9 :
+            payment +=5
+
+        else :
+            payment =+1
+
+            print (f"total payment is {patment}")
+
+
+class commision_clac_by_full_date():
+    exp_month = "07"
+    counter = 0
+    dates=["10-11-2024", "14-7-2024", "18-9-2024" ,"8-9-2024","3-9-2024","12-1-2024"]
+    for date in dates :
+        date_split = date.split("-")
+        month = date_split[1]
+        if month == exp_month ;
+        counter+=1
+
+        if counter >2 :
+            commision =0
+
+        else:
+            commision =5
+
+            print (f"total commitment is {commision}")
+            
