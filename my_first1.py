e257272a5c7dc3dc9c207d56269f6d774b5d0ee1
aslo <gzaltamara@gmail.com>
2026-04-24 14:31:15 -0700

    first commit

---
+++ my_first1.py
@@ -0,0 +1,16 @@
+#example of string
+first_name='tamara'
+last_name='asli'
+
+#example of integer
+salary=100
+tax=20
+legs_amount='2'
+vat='18'
+
+
+print (f"hi my name is {first_name} {last_name}")
+
+final_salary=salary-tax
+#vat_salary=salary+vat
+print(final_salary)
